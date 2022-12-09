57b24bfa4a57b1c845cb09c1aca5390335b5314a
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-08 22:24:55 -0600

    `feat`: was created 3 kind of container components

---
+++ components/app/NarrowContent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div 
+    class="overflow-hidden py-12 md:px-8 lg:px-16 container sm:container lg:container xl:container 2xl:container mx-auto dark:bg-slate-900">
+    <div class="px-3 md:px-8 lg:px-16 xl:px-24 ">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ components/app/Prose.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="-mx-4 px-7 prose prose-slate prose-fuchsia dark:prose-invert md:mx-auto">
+        <div class=" lg:px-3 py-6">
+            <slot />
+        </div>
+    </div>
+</template>
