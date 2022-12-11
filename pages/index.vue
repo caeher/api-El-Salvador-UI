593b38521a3bf1bbf8f0d7e05f5f67ef6798a322
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-10 21:34:54 -0600

    `fix`: was altered pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+const router = useRouter()
+function routerMap(maps: string | null) {
+    console.log(maps)
+    if (maps) {
+        router.push({
+            path: `/${maps}`
+        })
+    }
+}
+</script>
 <template>
     <div>
-        <h1>Hello World</h1>
+        <AppNarrowContent class="min-h-screen">
+            <AppProse>
+                <h1 class="text-center">
+                    El Salvador
+                </h1>
+                <MapElSalvador @maps="routerMap" class="w-full"></MapElSalvador>
+            </AppProse>
+        </AppNarrowContent>
     </div>
 </template>
