bfad1442095fcfd1e612df97b8685e515331d8fd
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-12 16:52:29 -0600

    `style`

--- app.vue
+++ app.vue
@@ -4,3 +4,12 @@
     <AppSpeedDial />
   </NuxtLayout>
 </template>
+
+<style lang="postcss">
+html, body {
+  @apply h-full;
+}
+body {
+  @apply bg-slate-100 dark:bg-slate-900/95;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { socialLinks } from '~~/utils/social-link'; 
+</script>
 <template>
+
     <slot />
+    <AppEmptyContainer />
+    <AppSocialNetwork :list="socialLinks"/>
+    <AppEmptyContainer />
+    <AppFooter />
 </template>
