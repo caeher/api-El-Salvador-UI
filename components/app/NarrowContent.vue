2ad3ef931deb8ff6762f6f4fa258e752fd7db799
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-20 21:03:58 -0600

    `style` update NarrowContent component

--- components/app/NarrowContent.vue
+++ components/app/NarrowContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div 
-    class="overflow-hidden py-12 md:px-8 lg:px-16 container sm:container lg:container xl:container 2xl:container mx-auto ">
+    class="overflow-none py-12 md:px-8 lg:px-16 container sm:container lg:container xl:container 2xl:container mx-auto my-12">
     <div class="px-3 md:px-8 lg:px-16 xl:px-24 ">
       <slot />
     </div>
