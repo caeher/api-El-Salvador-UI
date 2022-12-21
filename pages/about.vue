a63aba68bf70e6554e2fc8bc7cd435110bfd339d
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-20 21:16:12 -0600

    `fix`: update about page

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@ import { teams } from '~~/utils/teams';
                 <p>Api El Salvador es una iniciativa por parte de estudiantes de Ingeniería en Sistemas Informáticos modalidad en linea de la Universidad del El Salvador que pretende convertirse en una recopilacion de datos de El Salvador.</p>
                 <h2>Front End</h2>
                 <p>Es la parte de la interfaz de usuario lo que permite la navegación y visualización de todos los datos aquí recopilados</p>
-                <p>Para realizarlo se eligió <a href="https://nuxt.com/" target="_blank">NuxtJs</a>, un potente framework basado en Vuejs que permite la creacion de aplicaciones web multi-render por lo que si en un futuro se quire escala a algo m[as grande se podrá hacer creando nuevos nuxtModule</p>
+                <p>Para realizarlo se eligió <a href="https://nuxt.com/" target="_blank">NuxtJs</a>, un potente framework basado en Vuejs que permite la creacion de aplicaciones web multi-render por lo que si en un futuro se quire escala a algo más grande se podrá hacer creando nuevos nuxtModule</p>
                 <h2>Back End</h2>
                 ---
 
