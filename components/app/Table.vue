2650d880a6ebafe3a45d5666d2eb226fa16dc265
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-20 21:04:54 -0600

    `style`: update Table component

--- components/app/Table.vue
+++ components/app/Table.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 <template>
     <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs p-3 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th 
                         v-for="(item, index) in header"
