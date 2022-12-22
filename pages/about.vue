813ad22596e052a60822094d2bdbcf35998b27f4
StanDoge <stanlydoge@gmail.com>
2022-12-21 22:14:43 -0600

    `feat`: add Backend description in about page

--- pages/about.vue
+++ pages/about.vue
@@ -11,7 +11,13 @@ import { teams } from '~~/utils/teams';
                 <p>Es la parte de la interfaz de usuario lo que permite la navegación y visualización de todos los datos aquí recopilados</p>
                 <p>Para realizarlo se eligió <a href="https://nuxt.com/" target="_blank">NuxtJs</a>, un potente framework basado en Vuejs que permite la creacion de aplicaciones web multi-render por lo que si en un futuro se quire escala a algo más grande se podrá hacer creando nuevos nuxtModule</p>
                 <h2>Back End</h2>
-                ---
+
+                <p>Es la parte que se encarga de la comunicación con la base de datos que contiene la información sobre los departamentos y municipios del país </p>
+
+                <p>Para realizarlo se eligió a <a href="https://fastapi.tiangolo.com/" target="_blank">FastAPI </a>, un framework de Python para realizar API's con arquitectura REST el cual es igual de rápido que opciones en NodeJS como Express.js</p>
+
+                <p>Para la información de los códigos postales se desarrolló un scraper sencillo con <a href="https://beautiful-soup-4.readthedocs.io/en/latest/" target="_blank"> BeautifulSoupl</a> , una biblioteca para obtener información de archivos HTML y XML</p>
+
 
                 <h2>Colaboración</h2>
                 <p>Si deseas colaborar en este proyecto eres bienvenido, por favor leer esta seccion <NuxtLink href="/colaborate">colaborate</NuxtLink></p>
